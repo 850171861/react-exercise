cfd3348aaffd9e67313bd37b33297b194950a798
wdm <850171861@qq.com>
2021-08-15 22:51:05 +0800

    feat 添加工程列表

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React,{FormEvent} from "react";
 
 export const LoginScreen = () => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="username">用户名</label>
                 <input type="text" id={'username'} />
@@ -11,7 +14,7 @@ export const LoginScreen = () => {
                 <label htmlFor="password">密码</label>
                 <input type="password" id={'password'} />
             </div>
-            <button type={'submit'}></button>
+            <button type={'submit'}>登陆</button>
         </form>
     )
 }
