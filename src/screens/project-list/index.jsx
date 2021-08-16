3ee1b8d192d4001b78b6a40f5ffc19d08e141cf1
850171861 <850171861@qq.com>
2021-08-16 17:59:52 +0800

    fix 搜索框延迟请求

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,37 +2,39 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { cleanObject } from 'utils';
+import { cleanObject, useMount } from 'utils';
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
 export const ProjectListScreen = () => {
 
-    const [users,setUsers] = useState([])
+    const [users, setUsers] = useState([])
 
-    const [param,setParam] = useState({
-        name:'',
-        personId:''
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
     })
 
-    const [list,setList] = useState([])
+    const [list, setList] = useState([])
 
-    useEffect(()=>{
+
+
+    useEffect(() => {
         fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
-            if(response.ok){
-              setList(await response.json())
+            if (response.ok) {
+                setList(await response.json())
             }
         })
-    },[param])
+    }, [param])
 
-    useEffect(()=>{
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
-            if(response.ok){
-              setUsers(await response.json())
+            if (response.ok) {
+                setUsers(await response.json())
             }
         })
-    },[param])
+    })
 
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,19 @@
+import { useEffect } from 'react'
 export const isFaly = (value) => value === 0 ? false : !value
 export const cleanObject = (object) => {
-    const result = {...object}
+    const result = { ...object }
     Object.keys(result).forEach(key => {
         const value = result[key]
-        if(isFaly(value)){
+        if (isFaly(value)) {
             delete result[key]
         }
     })
 
     return result
+}
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback()
+    }, [])
 }
